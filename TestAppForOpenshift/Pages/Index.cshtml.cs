d8fcca20c5caed0c0c6dfbebedc234491acbd159
Hassan Ibrahim <54591195+HassanHamid201@users.noreply.github.com>
2020-10-02 09:01:53 +0300

    add  weather code

--- TestAppForOpenshift/Pages/Index.cshtml.cs
+++ TestAppForOpenshift/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using TestAppForOpenshift.Models;
 
 namespace TestAppForOpenshift.Pages
 {
@@ -16,10 +17,27 @@ namespace TestAppForOpenshift.Pages
         {
             _logger = logger;
         }
+         
+        [BindProperty]
+        public IEnumerable<WeatherForecast> WeatherForecasts { get; set; }
 
         public void OnGet()
         {
-
+            var rng = new Random();
+            WeatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
         }
+
+
+        private static readonly string[] Summaries = new[]
+      {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
     }
 }
